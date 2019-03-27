0076b76e36bceb1206c58440ec9d622f1aec22b1
kimjaehu <kimjaehu@gmail.com>
2019-03-27 19:28:05 +0000

    INITIALIZE project

--- app/javascript/components/NavBar.jsx
+++ app/javascript/components/NavBar.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class NavBar extends Component {
-  render() {
+class NavBar extends React.Component {
+  render () {
     return (
       <nav className="navbar">
-        <img src="/assets/images/logo.png" href="/" className="navbar-logo">
+        this is logo
+        <img src="/assets/images/logo.png" href="/" className="navbar-logo" />
       </nav>
-    );
+    )
   }
 }
 
