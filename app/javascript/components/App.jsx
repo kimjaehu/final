7ea9f6813aaa8ce5ee111de194ffe17676b12ef5
kimjaehu <kimjaehu@gmail.com>
2019-03-27 18:43:36 +0000

    INITIALIZE project

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <NavBar />
+
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ app/javascript/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <img src="/assets/images/logo.png" href="/" className="navbar-logo">
+      </nav>
+    );
+  }
+}
+
+export default NavBar
