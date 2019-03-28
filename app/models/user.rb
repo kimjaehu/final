c3c0d7f731b9ac21ff62e3c86f18e0565a67efa3
weihansu <weihan.sw@gmail.com>
2019-03-28 14:21:43 +0000

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
   has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_201734) do
+ActiveRecord::Schema.define(version: 2019_03_28_030254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "aboute_mes", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carrers", force: :cascade do |t|
+    t.string "job_category"
+    t.string "job_title"
+    t.string "month_experience"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "phone_number"
+    t.string "postal_code"
+    t.string "street_address"
+    t.string "region"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
